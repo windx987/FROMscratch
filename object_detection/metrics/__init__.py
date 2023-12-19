0de3094447e3e101dea6d2b2826012023ba44905
windx987 <terawat.cc@gmail.com>
2023-12-19 12:34:55 +0700

    mAP metrics/unittest

--- object_detection/metrics/__init__.py
+++ object_detection/metrics/__init__.py
@@ -1,2 +1,3 @@
 from metrics.iou import intersection_over_union 
 from metrics.nms import nms 
+from metrics.mAP import mean_average_precision
--- object_detection/metrics/nms.py
+++ object_detection/metrics/nms.py
@@ -1,19 +1,32 @@
-import  torch
+import torch
 from metrics.iou import intersection_over_union
 
 def nms(bboxes, iou_threshold, threshold, box_format="corners"):
+    """
+    Does Non Max Suppression given bboxes
 
-    # prediction = [[1, 0.9, x1, y1, x2, y2]]
+    Parameters:
+        bboxes (list): list of lists containing all bboxes with each bboxes
+        specified as [class_pred, prob_score, x1, y1, x2, y2]
+        iou_threshold (float): threshold where predicted bboxes is correct
+        threshold (float): threshold to remove predicted bboxes (independent of IoU) 
+        box_format (str): "midpoint" or "corners" used to specify bboxes
+
+    Returns:
+        list: bboxes after performing NMS given a specific IoU threshold
+    """
+
     assert type(bboxes) == list
 
-    bboxes = [ box for box in bboxes if box[1] > threshold]
+    bboxes = [box for box in bboxes if box[1] > threshold]
     bboxes = sorted(bboxes, key=lambda x: x[1], reverse=True)
     bboxes_after_nms = []
 
     while bboxes:
         chosen_box = bboxes.pop(0)
+
         bboxes = [
-            box 
+            box
             for box in bboxes
             if box[0] != chosen_box[0]
             or intersection_over_union(
